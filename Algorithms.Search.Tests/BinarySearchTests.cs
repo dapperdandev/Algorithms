e0a1d5c201fc941dcada8be9aad648975e162aca
dapperdandev <dapperdandev@gmail.com>
2018-10-03 12:10:03 -0600

    More Structure Updates

--- Algorithms.Search.Tests/BinarySearchTests.cs
+++ Algorithms.Search.Tests/BinarySearchTests.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 
 namespace Algorithms.Search.Tests
 {
+    [TestFixture]
     public class BinarySearchTests
     {
         private readonly int[] _intArray;
--- Algorithms.Search.Tests/LinearSearchTests.cs
+++ Algorithms.Search.Tests/LinearSearchTests.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 
 namespace Algorithms.Search.Tests
 {
+    [TestFixture]
     public class LinearSearchTests
     {
         private readonly int[] _intArray;
